b705942abcee2c0d734c0d5cd04e90b962345a99
freezy <marek.viger@gmail.com>
2014-12-18 12:01:11 +0100

    Default package config file

---
+++ src/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'database' => 'GeoLite2-Country.mmdb', // set to false/null or empty when you want to use API calls
+    'userId' => 999999,
+    'licenseKey' => '000000000000',
+);
